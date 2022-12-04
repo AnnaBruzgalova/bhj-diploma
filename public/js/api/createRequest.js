084411d1e3be255db57095ea9b504375e5a75ef7
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2022-12-04 19:58:53 +0300

    первый вариант

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,24 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+    const { url, data, method, callback } = options;
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let formData = new FormData();
+    const arr = [];
+    for (let key in data) {
+        arr.push(`${key}=${data[key]}`);
+    }
+    for (let item in data) {
+        FormData.append(item, data[item]);
+    }
+    try {
+        xhr.open(method, resultUrl);
+        xhr.send(method === 'GET' ? null : formData);
+    } catch (e) {
+        callback(e);
+    }
+    xhr.onerror = function() {
+        callback(xhr.statusText, null);
+    }
 };
